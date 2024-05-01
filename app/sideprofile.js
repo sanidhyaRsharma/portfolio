b159e86c542da487c9c7b5dcb9812cfb93b8a993
Sanidhya Sharma <sanidhyarsharma@gmail.com>
2024-05-01 16:09:56 -0400

    Added base path to Image Component

--- app/sideprofile.js
+++ app/sideprofile.js
@@ -3,7 +3,7 @@ export default function SideBarProfile() {
     return (
         <div className="flex justify-center my-4">
             <Image
-            src = "img/assets/profile.jpg"
+            src = "portfolio/img/assets/profile.jpg"
             className = "rounded-full border-[12px] border-gray-500"
             width = {200}
             height = {200}
