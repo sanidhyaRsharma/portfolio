5611be27f520eee9d27622ff8f68cdcbab9ad30c
Sanidhya Sharma <sanidhyarsharma@gmail.com>
2024-05-01 20:35:38 -0400

    Added developer summary

--- app/intro.js
+++ app/intro.js
@@ -1,9 +1,23 @@
-export default function Intro(){
-    return (
-        <div id="home" className="text-center flex-grow">
-            <h1 className="text-7xl m-4"> Sanidhya Sharma </h1>
-            <hr />
-            <h1 className="text-2xl m-4">Backend Developer and Researcher</h1>
-        </div>
-    );
+export default function Intro() {
+  return (
+    <div id="home" className="text-center flex-grow">
+      <h1 className="text-7xl mx-4 my-10"> Sanidhya Sharma </h1>
+      <hr />
+      <div className="shadow-md shadow-gray-500 p-8 m-4  bg-slate-600 bg-opacity-70 flex flex-col rounded-3xl ">
+        <h1 className="text-xl m-4">
+          Hi! I am a <strong>backend developer</strong> and a <strong>research enthusiast</strong>.
+          </h1>
+        <h1 className="text-md m-4"> 
+          I am actively looking for Full Time Opportunities. I am adept at Java Spring Boot and Deep
+          Learning and I am an inquisitive and quick learner. I have worked for
+          a couple years at Deutsche Bank as a Senior Developer.
+        </h1>
+
+        <h1 className="text-md m-4">
+          I am a big fan of Chess and Dota 2! Shoot me a message if you'd like
+          to play a game!
+        </h1>
+      </div>
+    </div>
+  );
 }
