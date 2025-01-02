7407a527cfb5ce8a08191f3f9139575a8df20fc6
Sanidhya Sharma <sanidhyarsharma@gmail.com>
2025-01-02 11:22:54 -0500

    Updated page width

--- app/page.js
+++ app/page.js
@@ -1,60 +1,70 @@
-'use client'
+"use client";
 
-import SideBar from "./sidebar.js"
-import Projects from "./projects.js"
-import Contact from "./contact.js"
-import Intro from "./intro.js"
-import Skills from "./skills.js"
-import WorkExp from "./workex.js"
-import Education from "./education.js"
+import SideBar from "./sidebar.js";
+import Projects from "./projects.js";
+import Contact from "./contact.js";
+import Intro from "./intro.js";
+import Skills from "./skills.js";
+import WorkExp from "./workex.js";
+import Education from "./education.js";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useRef, useState, useEffect } from "react"
+import { useRef, useState, useEffect } from "react";
 
 export default function Home() {
-  
-  const [isOpen, setIsOpen] = useState(false);
-  const sidebarRef = useRef(null);
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+	const [isOpen, setIsOpen] = useState(false);
+	const sidebarRef = useRef(null);
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
-  function handleClickOutside(event) {
-    if (sidebarRef?.current && !sidebarRef?.current?.contains(event.target)) {
-      setIsOpen(false);
-    }
-  }
-  
+	function handleClickOutside(event) {
+		if (
+			sidebarRef?.current &&
+			!sidebarRef?.current?.contains(event.target)
+		) {
+			setIsOpen(false);
+		}
+	}
 
-  const bgStyle = {
-    backgroundImage:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/portfolio/img/assets/package.jpeg') "
-  };
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-  
-  return (
-    <main className="flex flex-row bg-fixed" style={bgStyle} >
-      <div className="fixed flex top-0 left-0 p-3 m-4 rounded-full bg-slate-400 bg-opacity-60 w-16 h-16 justify-center align-middle">
-          <button onClick={handleClick}><GiHamburgerMenu size={36} /></button>
-        </div>
-      <div className="flex flex-row">
-        <SideBar handleClick={handleClick} isOpen={isOpen} ref={sidebarRef}/>
-        {isOpen  && <div className="flex md:flex-shrink-0 w-0 md:w-[300px] md:p-4 "></div> }
+	const bgStyle = {
+		backgroundImage:
+			"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/portfolio/img/assets/package.jpeg') ",
+	};
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	};
 
-      
-        <div className="flex-1">
-          <Intro/> 
-          <WorkExp />
-          <Projects />
-          <Skills />
-          <Education />
-          <Contact />
-        </div>
+	return (
+		<main className="flex flex-row bg-fixed" style={bgStyle}>
+			<div className="fixed flex top-0 left-0 p-3 m-4 rounded-full bg-slate-400 bg-opacity-60 w-16 h-16 justify-center align-middle">
+				<button onClick={handleClick}>
+					<GiHamburgerMenu size={36} />
+				</button>
+			</div>
+			<div className="flex flex-row">
+				<SideBar
+					handleClick={handleClick}
+					isOpen={isOpen}
+					ref={sidebarRef}
+				/>
+				{isOpen && (
+					<div className="flex md:flex-shrink-0 w-0 md:w-[300px] md:p-4 "></div>
+				)}
 
-      </div>
-    </main>
-  );
+				<div className="flex flex-col items-center">
+					<div className="flex flex-col md:w-3/4 lg:w-2/3">
+						<Intro />
+						<WorkExp />
+						<Projects />
+						<Skills />
+						<Education />
+						<Contact />
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
